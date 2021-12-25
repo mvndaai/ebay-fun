d1e21f86e781b3fee51c178b8a213b7a20d69d70
Jason Mavandi <mvndaai@gmail.com>
2021-12-24 22:56:57 -0800

    sort search; add seller to summary

--- main.js
+++ main.js
@@ -8,18 +8,20 @@ const search = async (query, appToken) => {
 	console.log('searching for:', query)
 	return axios({
 		method: 'get',
-		url: `https://${ebayBaseURL}/buy/browse/v1/item_summary/search?q=${query}=&limit=2`,
+		url: `https://${ebayBaseURL}/buy/browse/v1/item_summary/search?q=${query}=&limit=30&sort=price`,
 		headers: {
 			'Authorization': `Bearer ${appToken}`,
 			'Content-Type': 'application/json'
 		}
 	})
 	.then(resp => {
-		let summary = resp.data.itemSummaries.map(v => ({
+		let summary = resp.data.itemSummaries
+		.map(v => ({
 			itemId: v.itemId,
 			title: v.title,
 			price: `${v.price.value} ${v.price.currency}`,
 			itemWebUrl: v.itemWebUrl,
+			seller: v.seller.username,
 		}));
 		console.log(summary);
 		return summary;
@@ -93,11 +95,11 @@ const listInventory = (userToken) => {
 (async () => {
 
 	// Search
-	// let query = 'Topps Project 70 Card 386 - 1990 Catfish Hunter by Claw Money GOLD FRAME'
-	// search(query)
+	let query = '2020 139 topps now christian pache'
+	search(query)
 
 	// Show inventory of a user
-	let userToken = await askForUserToken()
+	// let userToken = await askForUserToken()
 	// console.log(userToken)
 
 	// getUser(userToken)
